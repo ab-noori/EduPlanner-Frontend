a2ff41871b88eb0bb4aa8eaff09d30312521c721
gisa chris <gisachris24@gmail.com>
2023-10-06 12:00:36 -0200

    Fix Signup Related Error

--- src/shared/signup_auth_form.js
+++ src/shared/signup_auth_form.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { userSignup } from '../app/features/users/userSlice';
 import LoadingModal from './loading';
@@ -9,28 +8,33 @@ import ErrorMessage from './inputError';
 function AuthenticationForm({ buttonName }) {
   const [nameData, setNameData] = useState('');
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const status = useSelector((state) => state.user.status);
+  const [loader, setLoader] = useState(false);
   const message = 'User Name can\'t be Blank';
 
-  const handleFormSubmition = () => {
+  const handleFormSubmition = (e) => {
+    e.preventDefault();
     dispatch(userSignup(nameData))
       .then((response) => {
         const userInfo = {
           id: response.payload.id,
           name: response.payload.name,
         };
-
         return userInfo;
       })
       .then((userInfo) => {
         sessionStorage.setItem('status', 'true');
         sessionStorage.setItem('logged_user', JSON.stringify(userInfo));
+      })
+      .then(() => {
+        setLoader(true);
+      })
+      .then(() => {
+        setTimeout(() => {
+          setLoader(false);
+          window.location.reload();
+        }, 1000);
       });
-
-    if (sessionStorage.getItem('status') === 'true') {
-      navigate('home');
-    }
   };
 
   const handleTextInput = (e) => {
@@ -38,12 +42,12 @@ function AuthenticationForm({ buttonName }) {
   };
   return (
     <>
-      {status === 'loading' ? (<LoadingModal />) : null}
+      {status === 'loading' || loader === true ? (<LoadingModal />) : null}
 
-      <form action="" onSubmit={handleFormSubmition}>
+      <form action="" onSubmit={(e) => handleFormSubmition(e)}>
         <input type="text" className="name_input" value={nameData} onChange={(e) => handleTextInput(e)} placeholder="Enter your Name.." />
         {nameData === '' ? (<ErrorMessage message={message} />) : null}
-        <button type="submit" className="submit_button">{buttonName}</button>
+        <button type="submit" className="submit_button" disabled={!nameData}>{buttonName}</button>
       </form>
     </>
   );
